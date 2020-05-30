5f1fcb3b54868713432e8309872efe796fa33fb2
Josh Gustafson <josh@strange.gustafson.me>
2020-05-30 11:22:02 -0600

    interactive input

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 
 #include "async_converter.h"
 
@@ -6,5 +7,13 @@ int main(int argc, char *argv[]) {
     async_converter converter([](std::string const& original, std::string const& converted){
         std::cout << "'" << original << "' -> '" << converted << "'" << std::endl;
     });
-    converter.convert("Hello world !");
+    static const std::set<std::string> quit_keywords{"q",  "quit", "exit"};
+    for (std::string line; std::getline(std::cin, line);) {
+        if (quit_keywords.count(line)) {
+            converter.cancel();
+            return 0;
+        }
+        converter.convert(line);
+    }
+    return 0;
 }
